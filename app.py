44fbc65ab934c67612a596478a581301b802dd8a
Mathys <mathys.ledanois@viacesi.fr>
2024-01-23 00:42:36 +0100

    ajout commentaires + fonction qui calcule la moyenne de chaque données pour ensuite les insérer dans la table Releve de la BDD + suppresion de id_sonde car autoincrémentation

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
 import flask
 import sqlite3
 import json
+import datetime
 
 
 app = flask.Flask(__name__, template_folder='views')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+
+#fonction pour lire le fichier json et le mettre dans une liste
 def read_file():
     with open('JSON/data.json', 'r') as fichier:
         data = json.load(fichier)
     return data
+#fonction pour ecrire dans la base de donnée les données du fichier json
 def write():
     data=json.loads(read_file())
     connection = sqlite3.connect('DATABASE')
     cursor = connection.cursor()
     for i in range(len(data)):
-        id_sonde= data[i]["id_sonde"]
         date_sonde= data[i]["date_sonde"]
         temp_sonde= data[i]["temp_sonde"]
         humidite_sonde= data[i]["humidite_sonde"]
         pression_sonde= data[i]["pression_sonde"]
-        cursor.execute("""insert into Sonde(id_sonde,date_sonde,temp_sonde,humidite_sonde,pression_sonde) values (?,?,?,?,?);""",(id_sonde,date_sonde,temp_sonde,humidite_sonde,pression_sonde))
+        cursor.execute("""insert into Sonde(date_sonde,temp_sonde,humidite_sonde,pression_sonde) values (?,?,?,?,?);""",(date_sonde,temp_sonde,humidite_sonde,pression_sonde))
         connection.commit()
         connection.close()
+#fonction pour avoir la moyenne des données de la base de donnée et les mettre dans la table Releve
+def moyenne():
+    connection = sqlite3.connect('DATABASE')
+    cursor = connection.cursor()
+    moyenne_temp=cursor.execute("""select avg(temp_sonde) from Sonde;""")
+    moyenne_humidite=cursor.execute("""select avg(humidite_sonde) from Sonde;""")
+    moyenne_pression=cursor.execute("""select avg(pression_sonde) from Sonde;""")
+    cursor.execute("""insert into Releve(date_releve,moy_temp,moy_humidite,moy_pression) values (?,?,?,?);""",(datetime.datetime.now(),moyenne_temp,moyenne_humidite,moyenne_pression)) #datetime.datetime.now() sert a avoir la date et l'heure actuelle et donc la date du relevé
+    connection.commit()
+    connection.close()
 
 """
 Temporaire juste pour voir sans changer de fenetre
