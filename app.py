0c636c24064eab3c6febf3259d97e414ff6b1ac3
Mathys <mathys.ledanois@viacesi.fr>
2024-01-23 17:12:32 +0100

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 
 app = flask.Flask(__name__, template_folder='views', static_url_path='', static_folder='static')
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5004, debug=True)
 
 
 #fonction pour lire le fichier json et le mettre dans une liste
