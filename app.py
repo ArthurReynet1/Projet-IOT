a3f27ebd786ace975ccadf27201b47afbbbce348
Mathys <mathys.ledanois@viacesi.fr>
2024-01-24 10:35:52 +0100

    .

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ def list_sonde():
        table_sonde.append({
        "sonde_id":sonde[0],
        "sonde_name":sonde[1]})
+   print(table_sonde) 
    return flask.render_template('modification.html',table_sonde=table_sonde)
 
 
