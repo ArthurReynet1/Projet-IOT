b2f61aaf10f7f34683d9a673d89467f43e9c42c0
Thomas Peligry <thomas.peligry@viacesi.fr>
2024-01-25 14:11:44 +0100

    edited main.cpp on ESP_files

--- ESP_files/main.cpp
+++ ESP_files/main.cpp
@@ -33,142 +33,145 @@ void sendJson(float temp, float hum, float press);
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  Wire.pins(0, 2);
-  Wire.begin();
+    Wire.pins(0, 2);
+    Wire.begin();
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
-  delay(2000);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;
+    }
+    delay(2000);
 
-  bool status;
+    bool status;
 
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);
+    // default settings
+    // (you can also pass in a Wire library object like &Wire2)
+    status = bme.begin(0x76);
 
-  setupWifi();
+    setupWifi();
 }
 
 void loop()
 {
 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-
-  display.println("METEO");
-  display.setTextSize(1);
-  display.println();
-  display.print("Temp. = ");
-  display.print(bme.readTemperature());
-  avgTemp = avgTemp + bme.readTemperature();
-  display.println(" C");
-
-  display.print("Press. = ");
-  display.print(bme.readPressure() / 100.0F);
-  avgPress = avgPress + (bme.readPressure() / 100.0F);
-  display.println(" hPa");
-
-  display.print("Hum. = ");
-  display.print(bme.readHumidity());
-  avgHum = avgHum + bme.readHumidity();
-  display.println(" %");
-
-  display.println("ligne1");
-  display.println("ligne2");
-  display.display();
-  count++;
-  if (count == 50)
-  {
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(2);
-    display.println("PKG SENT");
+    display.setTextColor(WHITE);
+
+    display.println("METEO");
     display.setTextSize(1);
-    count = 0;
-    avgTemp = avgTemp / 50;
-    avgHum = avgHum / 50;
-    avgPress = avgPress / 50;
+    display.println();
     display.print("Temp. = ");
-    display.print(avgTemp);
+    display.print(bme.readTemperature());
+    avgTemp = avgTemp + bme.readTemperature();
     display.println(" C");
+
     display.print("Press. = ");
-    display.print(avgPress);
+    display.print(bme.readPressure() / 100.0F);
+    avgPress = avgPress + (bme.readPressure() / 100.0F);
     display.println(" hPa");
+
     display.print("Hum. = ");
-    display.print(avgHum);
+    display.print(bme.readHumidity());
+    avgHum = avgHum + bme.readHumidity();
     display.println(" %");
+
     display.print("local IP:");
     display.println(wifip);
     display.display();
-    sendJson(avgTemp, avgHum, avgPress);
-    delay(1000);
-    avgTemp = 0;
-    avgHum = 0;
-    avgPress = 0;
-  }
-  delay(50);
+    count++;
+    if (count == 50)
+    {
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.setTextSize(2);
+        display.println("PKG SENT");
+        display.setTextSize(1);
+        count = 0;
+        avgTemp = avgTemp / 50;
+        avgHum = avgHum / 50;
+        avgPress = avgPress / 50;
+        display.print("Temp. = ");
+        display.print(avgTemp);
+        display.println(" C");
+        display.print("Press. = ");
+        display.print(avgPress);
+        display.println(" hPa");
+        display.print("Hum. = ");
+        display.print(avgHum);
+        display.println(" %");
+        display.print("local IP:");
+        display.println(wifip);
+        display.display();
+        sendJson(avgTemp, avgHum, avgPress);
+        delay(1000);
+        avgTemp = 0;
+        avgHum = 0;
+        avgPress = 0;
+    }
+    delay(50);
 }
 void setupWifi()
 {
-  WiFi.begin(SSID, PASSWORD);
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.print("Connecting");
-  display.display();
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    display.print(".");
-
+    WiFi.begin(SSID, PASSWORD);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.print("Connecting");
     display.display();
-  }
-  display.println();
-  display.print("Connected, IP address: ");
-  display.println(WiFi.localIP());
-  wifip = WiFi.localIP();
-  display.display();
-  delay(5000);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        display.print(".");
+
+        display.display();
+    }
+    display.println();
+    display.print("Connected, IP address: ");
+    display.println(WiFi.localIP());
+    wifip = WiFi.localIP();
+    display.display();
+    delay(5000);
 }
-void sendJson(float temp, float hum, float press){
-  
-  JsonDocument jsonDocument;
-  JsonArray dataArray = jsonDocument.createNestedArray("data");
-  JsonObject tempObject = dataArray.createNestedObject();
-  tempObject["temperature"] = temp;
-  JsonObject humObject = dataArray.createNestedObject();
-  humObject["humidity"] = hum;  
-  JsonObject pressObject = dataArray.createNestedObject();
-  pressObject["pressure"] = press;
+void sendJson(float temp, float hum, float press)
+{
 
-  // Convertir l'objet JSON en chaîne JSON
-  String jsonString;
-  serializeJson(jsonDocument, jsonString);
- 
-  // Envoyer la requête POST au serveur Raspberry Pi
-  HTTPClient http;
-  WiFiClient client;
-  http.begin(client,"http://192.168.11.49:5000/writejson");
-  http.addHeader("Content-Type", "application/json");
-  int httpResponseCode = http.POST(jsonString);
+    JsonDocument jsonDocument;
+    JsonArray dataArray = jsonDocument.createNestedArray("data");
+    JsonObject tempObject = dataArray.createNestedObject();
+    tempObject["temperature"] = temp;
+    JsonObject humObject = dataArray.createNestedObject();
+    humObject["humidity"] = hum;
+    JsonObject pressObject = dataArray.createNestedObject();
+    pressObject["pressure"] = press;
 
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-  } else {
-    Serial.print("HTTP Request failed. Error code: ");
-    Serial.println(httpResponseCode);
-  }
+    // Convertir l'objet JSON en chaîne JSON
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
 
-  http.end();
+    // Envoyer la requête POST au serveur Raspberry Pi
+    HTTPClient http;
+    WiFiClient client;
+    http.begin(client, "http://192.168.11.49:5000/writejson");
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.POST(jsonString);
 
+    if (httpResponseCode > 0)
+    {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+    }
+    else
+    {
+        Serial.print("HTTP Request failed. Error code: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();
 }
